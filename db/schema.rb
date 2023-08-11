1f4de38578138535d21f1b34313305540241f2d7
tatsuro0329 <yagyu.0329@gmail.com>
2023-08-11 11:07:18 +0900

    ユーザー管理機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_02_122359) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_003243) do
   create_table "articles", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "text", null: false
@@ -21,6 +21,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_122359) do
     t.integer "days_to_ship_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "birth_day"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
