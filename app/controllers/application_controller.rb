b1ccb4aa3f0dac13a9ede7d01e770e1dbb4556b2
tatsuro0329 <yagyu.0329@gmail.com>
2023-08-11 20:14:09 +0900

    テストコードの編集-ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :fist_name, :last_name_kana, :first_name_kana, :birth_day])
   end
 
   private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+    it "nickname,email,password,password_confirmation,last_name,first_name,last_name_kana,first_name_kana,birth_dayが存在すれば登録できる" do
+    end
+
+    context '新規登録できない場合' do
     it 'nicknameが空では登録できない' do
-      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      # emailが空では登録できないテストコードを記述します
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'password_confirmationが空では登録できない' do
+    end
+    it 'last_nameが空では登録できない' do
+    end
+    it 'frist_nameが空では登録できない' do
+    end
+    it 'last_name_kanaが空では登録できない' do
+    end
+    it 'frist_name_kanaが空では登録できない' do
+    end
+    it 'birth_dayが空では登録できない' do
+    end
+    it 'last_nameは全角（漢字・ひらがな・カタカナ）以外では登録できない' do
+    end
+    it 'frist_nameは全角（漢字・ひらがな・カタカナ）以外では登録できない' do
+    end
+    it 'last_name_kanaは全角（カタカナ）以外では登録できない' do
+    end
+    it 'frist_name_kanaは全角（カタカナ）以外では登録できない' do
     end
   end
 end
+
