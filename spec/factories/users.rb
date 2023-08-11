114eaef61674d33f017dabff98309a15c1c6e374
tatsuro0329 <yagyu.0329@gmail.com>
2023-08-12 00:08:47 +0900

    テストコードの編集3-ユーザー管理機能

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,15 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
-    password_confirmation {password}
-    last_name
-    first_name
-    last_name_kana
-    first_name_kana
-    birth_day
+    japanese_user = Gimei.name
+
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name            { japanese_user.first.kanji }
+    first_name_kana       { japanese_user.first.katakana }
+    last_name             { japanese_user.last.kanji }
+    last_name_kana        { japanese_user.last.katakana }
+    birth_day             { Faker::Date.birthday(min_age: 5, max_age: 93) }
   end
 end
