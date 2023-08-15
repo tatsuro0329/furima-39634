e20e42bfd98a8749105cd77ee2fd4c621388b22f
tatsuro0329 <yagyu.0329@gmail.com>
2023-08-15 20:12:46 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_log_in, only: [:new]
 
   def index
+    @items = Item.includes(:user).order("created_at DESC")
     @items = Item.all
   end
 
