6754b73367d9b4d5e6ace3d88720ad6fd3e0fe28
tatsuro0329 <yagyu.0329@gmail.com>
2023-08-16 16:22:45 +0900

    Revert "Revert "商品編集機能""
    This reverts commit 540ae6d43b844649d4462bcb3352100439478376.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_log_in, only: [:new]
+  before_action :move_to_log_in, only: [:new, :edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -25,11 +25,21 @@ class ItemsController < ApplicationController
   # def destroy
   # end
 
-  # def edit
-  # end
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path(@item)
+    end
+  end
 
-  # def update
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
   def move_to_log_in
